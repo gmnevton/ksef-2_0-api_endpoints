7edcc5cc06f6cde754ae8a86440d8e4295ef02b9
gmnevton <gmnevton@gmail.com>
2025-10-20 14:40:22 +0200

    Added commands to create self-signed certificate using OpenSSL, and using KSeF.Client.Tests.CertTestApp for dotNET 9.0.

---
+++ doNET-KSeF.Client.Tests.CertTestApp/app-run.bat
@@ -0,0 +1,1 @@
+dotnet run --framework net9.0 --output file --nip 8976111986 --no-startup-warnings
---
+++ doNET-KSeF.Client.Tests.CertTestApp/github-clone.bat
@@ -0,0 +1,2 @@
+git clone https://github.com/CIRFMF/ksef-client-csharp.git
+cd ksef-client-csharp/KSeF.Client.Tests.CertTestApp
