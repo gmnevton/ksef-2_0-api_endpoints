81369afbd7e4d1e599f49941bafaa77bdb0e0dc0
gmnevton <gmnevton@gmail.com>
2025-11-03 13:28:55 +0100

    Updated to version RC5.6. And some other minor changes.

--- doNET-KSeF.Client.Tests.CertTestApp/app-run.bat
+++ doNET-KSeF.Client.Tests.CertTestApp/app-run.bat
@@ -1,1 +1,2 @@
+cd ksef-client-csharp/KSeF.Client.Tests.CertTestApp
 dotnet run --framework net9.0 --output file --nip 8976111986 --no-startup-warnings
--- doNET-KSeF.Client.Tests.CertTestApp/github-clone.bat
+++ doNET-KSeF.Client.Tests.CertTestApp/github-clone.bat
@@ -1,2 +1,1 @@
 git clone https://github.com/CIRFMF/ksef-client-csharp.git
-cd ksef-client-csharp/KSeF.Client.Tests.CertTestApp
